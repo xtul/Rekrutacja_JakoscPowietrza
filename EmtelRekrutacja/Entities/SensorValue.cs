3daf86b7a55a8a5fb7a5cf62ef4a4b8a89a70583
Michał Piasecki <michal@elexpress.pl>
2021-03-31 15:51:20 +0200

    Updated SensorValue class as requested. Changed System.Text.Json.Serialization.JsonPropertyName to Newtonsoft.Json.JsonProperty. Made Value.Amount a nullable property. Removed JsonPropertyName attribute for SensorValue.Values, as it was unnecessary.

--- EmtelRekrutacja/Entities/SensorValue.cs
+++ EmtelRekrutacja/Entities/SensorValue.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -7,13 +8,12 @@ using System.Threading.Tasks;
 namespace EmtelRekrutacja.Entities {
 	public class Value {
 		public string Date { get; set; }
-		[JsonPropertyName("value")]
-		public double Amount { get; set; }
+		[JsonProperty("value")]
+		public double? Amount { get; set; }
 	}
 
 	public class SensorValue {
 		public string Key { get; set; }
-		[JsonPropertyName("values")]
 		public List<Value> Values { get; set; }
 	}
 }
